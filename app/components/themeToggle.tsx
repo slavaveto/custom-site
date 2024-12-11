7aeb6f684677e5a4572ca08413418031dc54a1df
Aro <aro.jormand@gmail.com>
2024-12-11 20:37:12 +0100

    themeToggle update

--- app/components/themeToggle.tsx
+++ app/components/themeToggle.tsx
@@ -35,20 +35,6 @@ export const ThemeModal = () => {
     }
   }, [colorMode]);
 
-  const toggleDarkMode = () => {
-    setIsDarkMode((prevMode) => {
-      const newMode = !prevMode;
-      if (newMode) {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-      }
-      return newMode;
-    });
-  };
-
   return (
     <>
       <div
