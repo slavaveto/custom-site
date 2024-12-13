aba0de638359be5d5f7fcae76fabaf34a55aebb9
Aro <aro.jormand@gmail.com>
2024-12-13 21:41:43 +0100

    burger-fix

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -13,12 +13,14 @@ export const Header = () => {
   return (
     <header className="h-[81px] w-full bg-gray-300 dark:bg-gray-900 flex items-center justify-center text-black dark:text-white">
       <div className="flex items-center w-full max-w-[800px] px-4">
-        <Hamburger
-          toggled={mobileNavOpen}
-          toggle={() => setMobileNavOpen(!mobileNavOpen)}
-          direction="left"
-          duration={0.5}
-        />
+        <div className="md:hidden">
+          <Hamburger
+            toggled={mobileNavOpen}
+            toggle={() => setMobileNavOpen(!mobileNavOpen)}
+            direction="left"
+            duration={0.5}
+          />
+        </div>
 
         <div className="ml-auto">
           <Link href="/">
